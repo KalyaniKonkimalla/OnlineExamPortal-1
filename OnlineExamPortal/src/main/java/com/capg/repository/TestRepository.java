e5513890f62a2521a4075eaa60c99a4d40a35c6e
SriramSameer7 <97208316+SriramSameer7@users.noreply.github.com>
2022-01-29 22:27:03 +0530

    Delete OnlineExamPortal directory

--- OnlineExamPortal/src/main/java/com/capg/repository/TestRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capg.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.capg.model.test;
-
-public interface TestRepository extends JpaRepository<test,Long> {
-
-}
